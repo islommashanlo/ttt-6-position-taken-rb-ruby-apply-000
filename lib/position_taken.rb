e47b7f61989f6f7cf2672764bcc5751934188a47
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-14 15:26:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " " or board[index] == "" or board[index] == nil
+  if board[index] == " "
+    or board[index] == ""
+    or board[index] == nil
     then return false
   end
 end
